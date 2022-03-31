1bab5fff2de34d7f685c8794f5e802bdea3ded72
Christian Steven Ramirez Devia <steven.ramirez@ceiba.com.co>
2022-03-31 16:52:34 -0500

    Pruebas unitarias a la logica de negocio

--- gastos-viajes/aplicacion/src/main/java/com/ceiba/gastoviaje/consulta/ManejadorReportesGastoViaje.java
+++ gastos-viajes/aplicacion/src/main/java/com/ceiba/gastoviaje/consulta/ManejadorReportesGastoViaje.java
@@ -1,11 +1,9 @@
 package com.ceiba.gastoviaje.consulta;
 
 import com.ceiba.gastoviaje.modelo.dto.DtoReporteGastosViajes;
-import com.ceiba.gastoviaje.puerto.repositorio.RepositorioGastoViaje;
 import com.ceiba.gastoviaje.servicio.ServicioReportesGastoViaje;
 import org.springframework.stereotype.Component;
 
-import java.util.List;
 
 @Component
 public class ManejadorReportesGastoViaje {
--- gastos-viajes/dominio/src/test/java/com/ceiba/gastoviaje/servicio/ServicioReportesGastoViajeTest.java
+++ gastos-viajes/dominio/src/test/java/com/ceiba/gastoviaje/servicio/ServicioReportesGastoViajeTest.java
@@ -4,6 +4,7 @@ import com.ceiba.BasePrueba;
 import com.ceiba.constantes.ResponsablePagoConstantes;
 import com.ceiba.dominio.excepcion.ExcepcionDuplicidad;
 import com.ceiba.gastoviaje.modelo.dto.DtoEmpleadoDetalleGasto;
+import com.ceiba.gastoviaje.modelo.dto.DtoReporteGastosViajes;
 import com.ceiba.gastoviaje.puerto.repositorio.RepositorioGastoViaje;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -54,5 +55,21 @@ public class ServicioReportesGastoViajeTest {
         assertEquals(ResponsablePagoConstantes.SURA, empleadoDetalleGastoList.get(1).getResponsablePago());
         assertEquals(ResponsablePagoConstantes.SURA, empleadoDetalleGastoList.get(2).getResponsablePago());
         assertEquals(ResponsablePagoConstantes.SURA, empleadoDetalleGastoList.get(3).getResponsablePago());
+    }
+
+    @Test
+    void deberiaGenerarConExitoElReporteDeGastosDeViajeDeLosEmpleados() {
+        // arrange
+        RepositorioGastoViaje repositorioGastoViaje = Mockito.mock(RepositorioGastoViaje.class);
+        Mockito.when(repositorioGastoViaje.existeAlguno()).thenReturn(true);
+        Mockito.when(repositorioGastoViaje.obtenerTodosConEmpleadoNombre()).thenReturn(empleadoDetalleGastoList);
+        ServicioReportesGastoViaje servicioReportesGastoViaje = new ServicioReportesGastoViaje(repositorioGastoViaje);
+        // act
+        DtoReporteGastosViajes dtoReporteGastosViajes = servicioReportesGastoViaje.generarReporteGastosViajesEmpleados();
+        // assert
+        assertNotNull(dtoReporteGastosViajes);
+        assertEquals(new BigDecimal("3250000"), dtoReporteGastosViajes.getValorTotalGastos());
+        assertEquals(4, dtoReporteGastosViajes.getEmpleadosDetalleGastos().size());
+        Mockito.verify(repositorioGastoViaje, Mockito.times(1)).obtenerTodosConEmpleadoNombre();
     }
 }
